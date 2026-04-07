f7d31855607d247725334e29d1fc3591aacca862
espinelcaceresmariafernanda-svg <espinelcaceresmariafernanda@gmail.com>
2026-04-07 16:40:58 -0500

    Add files via upload

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,32 +1,20 @@
-$(function()
+$(function(){
     const socket = io();
-
-    const $messageForm = $('#message-form');
-    const $messageBox = $('#message');
-    const $chat = $('#chat');
-
-    const $nickForm = $('#nickForm');
-    const $nickError = $('#nickError');
-    const $nickname = $('#nickname');
-    const $users = $('#usernames');
-
+    const $messageForm = $('#message-form'), $messageBox = $('#message'), $chat = $('#chat');
+    const $nickForm = $('#nickForm'), $nickError = $('#nickError'), $nickname = $('#nickname'), $users = $('#usernames');
     let myNickname = '';
 
     $nickForm.submit( e => {
         e.preventDefault();
         const nick = $nickname.val().trim();
-        if(nick === ''){
-            $nickError.html('<div class="alert alert-warning">El nombre no puede estar vacío.</div>');
-            return;
-        }
-
         socket.emit('nuevo usuario', nick, data => {
             if(data){
                 myNickname = nick;
+                // ESTA LÍNEA ES LA QUE QUITA EL "DISPLAY: NONE" DEL CSS
                 $('#nickWrap').hide();
                 $('#contentWrap').show();
             } else {
-                $nickError.html('<div class="alert alert-danger">El nombre de usuario ya existe.</div>');
+                $nickError.html('<div class="alert alert-danger">El nombre ya existe.</div>');
             }
             $nickname.val('');
         });
@@ -34,24 +22,27 @@ $(function()
 
     $messageForm.submit( e => {
         e.preventDefault();
-        const msg = $messageBox.val().trim();
-        if(msg === '') return;
-
-        // Enviamos el mensaje y añadimos una función para recibir errores (como "usuario no existe")
-        socket.emit('Enviar mensaje', msg, data => {
-            $chat.append(`<div class="msg-system text-danger"><em>${data}</em></div>`);
-            $chat.scrollTop($chat[0].scrollHeight);
+        socket.emit('Enviar mensaje', $messageBox.val(), data => {
+            $chat.append(`<p style="color:red"><em>${data}</em></p>`);
         });
         $messageBox.val('');
     });
 
-    socket.on('Nuevo mensaje', function (data){
-        const isMe = data.nick === myNickname;
-        const msgHtml = isMe
-            ? `<div class="msg msg-me"><span class="msg-nick">Tú</span> ${data.msg}</div>`
-            : `<div class="msg msg-other"><span class="msg-nick">${data.nick}</span> ${data.msg}</div>`;
-        $chat.append(msgHtml);
+    socket.on('Nuevo mensaje', data => {
+        $chat.append(`<div><b>${data.nick}:</b> ${data.msg}</div>`);
         $chat.scrollTop($chat[0].scrollHeight);
     });
 
-    // === AQUÍ ESTÁ EL ARREGLO PARA LOS MENSAJES PRIV
+    socket.on('whisper', data => {
+        $chat.append(`<div style="color:gray; font-style:italic"><b>${data.nick} (privado):</b> ${data.msg}</div>`);
+        $chat.scrollTop($chat[0].scrollHeight);
+    });
+
+    socket.on('usernames', data => {
+        let html = '';
+        for(let i = 0; i < data.length; i++) {
+            html += `<p><i class="fas fa-user"></i> ${data[i]}</p>`;
+        }
+        $users.html(html);
+    });
+});
